62fd64d152c4f4b445caefa25e267a2640639098
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-01-26 13:17:14 +0800

    Update Transcript_Analysis.py

--- pages/Transcript_Analysis.py
+++ pages/Transcript_Analysis.py
@@ -8,8 +8,8 @@ def main():
     link = "https://filecache.investorroom.com/mr5ir_hysteryale/942/hy-us-investor-day-transcript-20231116.pdf"
     st.markdown(f"[Click here for the link to the transcript]({link})", unsafe_allow_html=True)
 
-    st.write('Background Information')
-    st.text('Industry and Key Developments: Hyster-Yale Materials Handling Inc. operates in three main business segments: Lift Truck, Bolzoni attachment, and Nuvera fuel cell businesses. The Lift Truck business is the core, while Bolzoni is closely associated with lift trucks, and Nuvera focuses on decarbonization green power solutions for forklift trucks and related industries​​.')
+    st.markdown('## Background Information')
+    st.write('Industry and Key Developments: Hyster-Yale Materials Handling Inc. operates in three main business segments: Lift Truck, Bolzoni attachment, and Nuvera fuel cell businesses. The Lift Truck business is the core, while Bolzoni is closely associated with lift trucks, and Nuvera focuses on decarbonization green power solutions for forklift trucks and related industries​​.')
     
 
 if __name__ == "__main__":
