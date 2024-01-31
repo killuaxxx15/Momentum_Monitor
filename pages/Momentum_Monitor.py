9c55ea0cc87c56bc9747f1881aaf49fb7ee8a122
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-01-31 15:29:50 +0800

    Update Momentum_Monitor.py

--- pages/Momentum_Monitor.py
+++ pages/Momentum_Monitor.py
@@ -15,6 +15,28 @@ def color_cells(val):
     color = '#ffcccc' if val == 'CASH' else ('#ccffcc' if val == 'INVESTED' else '')
     return f'background-color: {color}'
 
+# Function to format as whole number percentage
+def format_percentage_whole(val):
+    if isinstance(val, (int, float)):
+        return '{:.0%}'.format(val)
+    return val
+
+# Function to format as percentage with at most 1 decimal place
+def format_percentage_one_decimal(val):
+    if isinstance(val, (int, float)):
+        return '{:.1%}'.format(val)
+    return val
+
+# Define the yellow background styling function
+def yellow_background(val):
+    return 'background-color: yellow'
+
+# Custom formatting function
+def custom_format(val):
+    if isinstance(val, float):
+        return f"{val:.1f}" if val != int(val) else f"{int(val)}"
+    return val
+
 # Function to read data from Excel and apply styling
 def read_excel_and_style(sheet_name, usecols, header, nrows):
     df = pd.read_excel(excel_file,
@@ -33,25 +55,20 @@ with st.expander("Table 1"):
     st.dataframe(df1, hide_index=True)
 
 with st.expander("Table 2"):
-    df2 = read_excel_and_style(sheet_name, 'E:J', 13, 14)
-    
-    # List of columns to be converted to integers
-    columns_to_int = ['3 month return', '6 month rank']
-    
-    # Iterate through columns and fill NaN with empty string for non-numeric columns
-    for col in df2.columns:
-        try:
-            if col not in columns_to_int:
-                df2[col] = df2[col].fillna('')
-        except Exception as e:
-            st.warning(f"An error occurred while processing column '{col}': {e}")
+    df3 = read_excel_and_style(sheet_name, 'E:O', 29, 10)
 
-    # Convert '3 month return' and '6 month rank' to integers
-    # First replace NaN with 0 or any other default integer value
-    df2['3 month return'] = df2['3 month return'].fillna(0).round().astype(int)
-    df2['6 month rank'] = df2['6 month rank'].fillna(0).round().astype(int)
-
-    st.dataframe(df2, hide_index=True)
+    # Apply the formatting to the specific columns
+    df3['Breadth'] = df3['Breadth'].apply(format_percentage_whole)
+    df3['Closeness to 52 week'] = df3['Closeness to 52 week'].apply(format_percentage_one_decimal)
+    df3['U/D'] = df3['U/D'].apply(format_percentage_one_decimal)
+    
+    # Apply styling and custom formatting to the DataFrame
+    df3 = df3.style.applymap(yellow_background, subset=['median']).format({
+        '3 month return': '{:.1f}',  # Format '3 month return' with 1 decimal place
+        'median': custom_format  # Apply custom formatting to 'median' column
+    })
+    
+    st.dataframe(df3, hide_index=True)
 
 
 
