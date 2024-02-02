ba060a3f1fa7d5c7afb869f64a0246b22b19e5f3
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-02-02 13:18:02 +0800

    Update Momentum_Monitor.py

--- pages/Momentum_Monitor.py
+++ pages/Momentum_Monitor.py
@@ -37,14 +37,14 @@ df2 = pd.read_excel(excel_file,
                    nrows=13)
 
 # Apply the styling
-df2 = df2.style.applymap(color_cells)
+styled_df = df2.style.applymap(color_cells)
 
-# Convert styled DataFrame to HTML
-df2 = df2.to_html(escape=False, index=False)
+# Convert styled DataFrame to HTML without including the index
+df_html = styled_df.to_html(escape=False, index=False)
 
 # Display the HTML in Streamlit
 st.markdown('#### Table 2: Relative Ranking', unsafe_allow_html=True)
-st.markdown(df2, unsafe_allow_html=True)
+st.markdown(df_html, unsafe_allow_html=True)
 
 
 
