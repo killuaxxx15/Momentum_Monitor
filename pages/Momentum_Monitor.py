878bd15ec7cbae9474e9ecbf7bbc31f555ccff1d
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-02-02 13:06:25 +0800

    Update Momentum_Monitor.py

--- pages/Momentum_Monitor.py
+++ pages/Momentum_Monitor.py
@@ -40,10 +40,10 @@ df2 = pd.read_excel(excel_file,
 #df2 = df2.style.applymap(color_cells)
 st.markdown('#### Table 2: Relative Ranking')
 # Convert DataFrame to markdown without including the index
-df_markdown = df2.to_markdown(index=False)
+df2 = df2.to_markdown(index=False)
 
 # Display the markdown in Streamlit
-st.markdown(df_markdown, unsafe_allow_html=True)
+st.markdown(df2, unsafe_allow_html=True)
 
 
 
