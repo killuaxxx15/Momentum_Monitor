39fb4336832d2e3195288194c01fe19effe783a9
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-02-09 16:31:40 +0800

    Update Momentum_Monitor_India_ETF.py

--- pages/Momentum_Monitor_India_ETF.py
+++ pages/Momentum_Monitor_India_ETF.py
@@ -31,12 +31,9 @@ df1 = pd.read_excel(excel_file,
 df1 = df1.rename(columns={'Unnamed: 3' : 'TICKER'})
 df1 = df1.rename(columns={'Unnamed: 4' : 'ETF'})
 df1 = df1.rename(columns={'Unnamed: 5' : 'Relative Ranking'})
-# Applying the styling to the DataFrame
-#df1 = df1.style.applymap(color_cells, subset=['Above 30 D ', 'Above 60 D', 'Above 200D'])
-#df1 = df1.style.applymap(color_cells_1, subset=['Unnamed: 6'])
 df1 = df1.style.applymap(color_cells, subset=['Above 30 D ', 'Above 60 D', 'Above 200D'])\
-               .applymap(color_cells_1, subset=['Unnamed: 6'])
-
+               .applymap(color_cells_1, subset=['Relative Ranking'])
+df1 = df1.drop(['Unnamed: 6'], axis=1)
 st.markdown('### Relative Ranking')
 st.dataframe(df1, hide_index=True)
 
