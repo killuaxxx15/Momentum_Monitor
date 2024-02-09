7034f9d22eb04cf26706d1ac29317be2d53bc195
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-02-09 15:40:04 +0800

    Update Momentum_Monitor_India_ETF.py

--- pages/Momentum_Monitor_India_ETF.py
+++ pages/Momentum_Monitor_India_ETF.py
@@ -15,7 +15,7 @@ def color_cells(val):
     color = '#ffcccc' if val == 'CASH' else ('#ccffcc' if val == 'INVESTED' else '')
     return f'background-color: {color}'
 
-
+# TABLE 1
 df1 = pd.read_excel(excel_file,
                    sheet_name=sheet_name,
                    usecols='D:J',
@@ -29,5 +29,23 @@ df1 = df1.rename(columns={'Unnamed: 5' : 'Relative Ranking'})
 df1 = df1.style.applymap(color_cells)
 st.markdown('#### Relative Ranking')
 st.dataframe(df1, hide_index=True)
+
+
+
+# TABLE 2
+df2 = pd.read_excel(excel_file,
+                   sheet_name=sheet_name,
+                   usecols='D:P',
+                   header=23,
+                   nrows=14)
+
+st.markdown('### Equity Ranking: Momentum + Breadth + Upgrades')
+st.dataframe(df2, hide_index=True)
+
+
+
+
+
+
 
 
