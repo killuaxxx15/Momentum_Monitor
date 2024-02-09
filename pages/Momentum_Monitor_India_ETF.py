4a368f52e211f4db8b97b792dd8b443eca4b2635
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-02-09 15:33:41 +0800

    Update Momentum_Monitor_India_ETF.py

--- pages/Momentum_Monitor_India_ETF.py
+++ pages/Momentum_Monitor_India_ETF.py
@@ -1,3 +1,30 @@
+import pandas as pd
 import streamlit as st
 
+st.set_page_config(page_title='India ETF Momentum',
+                   page_icon=':bar_chart:')
+
 st.header('Momentum Monitor India ETF')
+
+excel_file = 'INDIA_ETF_MOMENTUM.xlsx'
+sheet_name = 'Aset class Rankings'
+
+
+# Function to apply color based on cell value
+def color_cells(val):
+    color = '#ffcccc' if val == 'CASH' else ('#ccffcc' if val == 'INVESTED' else '')
+    return f'background-color: {color}'
+
+
+df1 = pd.read_excel(excel_file,
+                   sheet_name=sheet_name,
+                   usecols='D:J',
+                   header=5,
+                   nrows=14)
+
+# Applying the styling to the DataFrame
+df1 = df1.style.applymap(color_cells)
+st.markdown('#### Relative Ranking')
+st.dataframe(df1, hide_index=True)
+
+
