8bd9acbd843c3941ce6c72737490b590411cefc8
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-02-12 20:00:02 +0800

    Update Momentum_Monitor_US_ETF.py

--- pages/Momentum_Monitor_US_ETF.py
+++ pages/Momentum_Monitor_US_ETF.py
@@ -2,7 +2,6 @@ import pandas as pd
 import streamlit as st
 
 st.set_page_config(page_title='US ETF Momentum Monitor',
-                   page_icon=':bar_chart:',
-                   layout='wide')
+                   page_icon=':bar_chart:')
 
-st.markdown("<div style='text-align: center;'> <h1>Momentum Monitor US ETF</h1> </div>", unsafe_allow_html=True)
+st.header('Momentum Monitor US ETF')
