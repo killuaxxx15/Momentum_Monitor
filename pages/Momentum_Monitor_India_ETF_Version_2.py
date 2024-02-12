086ff203144ac1ef357984abcfeed3ffa9877a81
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-02-12 17:50:53 +0800

    Update Momentum_Monitor_India_ETF_Version_2.py

--- pages/Momentum_Monitor_India_ETF_Version_2.py
+++ pages/Momentum_Monitor_India_ETF_Version_2.py
@@ -2,8 +2,8 @@ import pandas as pd
 import streamlit as st
 
 st.set_page_config(page_title='India ETF Momentum',
-                   page_icon=':bar_chart:,
-                   layout="wide"')
+                   page_icon=':bar_chart:',
+                   layout="wide")
 
 #st.header('Momentum Monitor India ETF')
 st.markdown("<div style='text-align: center;'> <h1>Momentum Monitor India ETF</h1> </div>", unsafe_allow_html=True)
