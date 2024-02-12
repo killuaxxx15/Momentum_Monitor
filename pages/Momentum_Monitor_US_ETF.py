229e46b23d977e354afb0fcc86d79707f6a0aed1
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-02-12 16:05:52 +0800

    Update Momentum_Monitor_US_ETF.py

--- pages/Momentum_Monitor_US_ETF.py
+++ pages/Momentum_Monitor_US_ETF.py
@@ -26,6 +26,7 @@ images_col = {"Images": [
 
 df_images = pd.DataFrame(images_col)
 aa = df_images['Images']
+aa = aa.to_html(escape=False)
 st.markdown(aa, unsafe_allow_html=True)
 #df_images_html = df_images.to_html(escape=False)
 #st.markdown(df_images_html, unsafe_allow_html=True)
