8164e235a9d620576ff786ce037dae8c6386785f
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-02-12 12:23:25 +0800

    Create Momentum_Monitor_US_ETF.py

---
+++ pages/Momentum_Monitor_US_ETF.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(page_title='US ETF Momentum Monitor',
+                   page_icon=':bar_chart:')
+
+st.header('Momentum Monitor US ETF')
