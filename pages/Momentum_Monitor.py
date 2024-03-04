b90b86a289a6c0226a4526b97fad0defc3e97f92
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-03-04 16:44:24 +0800

    Update Momentum_Monitor.py

--- pages/Momentum_Monitor.py
+++ pages/Momentum_Monitor.py
@@ -33,7 +33,7 @@ def color_cells_2(val):
     return f'background-color: {color}'
   
 def color_circle_1(val):
-    if val > 2:
+    if val >= 2:
         return '🔴'  
     elif val < 0:
         return '🟢'  
@@ -101,7 +101,7 @@ relative_ranking = df3['Relative Ranking']
 df3 = df3.drop(['Relative Ranking'], axis=1)
 df3 = df3.drop(['Unnamed: 9'], axis=1)
 df3.insert(1, "Relative Ranking", relative_ranking)
-#df3['Relative Ranking'] = df3['Relative Ranking'].apply(color_circle_1)
+df3['Relative Ranking'] = df3['Relative Ranking'].apply(color_circle_1)
 df3 = df3.style.format({
       'U/D': percent_one_decimal, 
       'Breadth': percent_whole_number, 
