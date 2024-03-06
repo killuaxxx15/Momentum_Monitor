4b16a0855e6568f50a2734645c99b5d2e13e61f4
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-03-06 18:35:31 +0800

    Update ETF_Model_EM.py

--- pages/ETF_Model_EM.py
+++ pages/ETF_Model_EM.py
@@ -18,19 +18,8 @@ df1 = pd.read_excel(excel_file,
                    header=5,
                    nrows=21)
 
-st.markdown('### Table 1: OUTPUT_COUNTRY')
+st.markdown('### OUTPUT COUNTRY')
 st.dataframe(df1, hide_index=True)
 
 
 
-# Sample data
-data = {
-    ('Group Name', 'Subcolumn 1'): [1, 2, 3],
-    ('Group Name', 'Subcolumn 2'): [4, 5, 6]
-}
-
-# Create a DataFrame with a MultiIndex for the columns
-df = pd.DataFrame(data)
-
-# Display the DataFrame in Streamlit
-st.dataframe(df)
