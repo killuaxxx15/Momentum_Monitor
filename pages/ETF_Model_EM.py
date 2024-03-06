37a7507b6f660a38d2c059da2495d0916eabfa44
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-03-06 18:41:37 +0800

    Update ETF_Model_EM.py

--- pages/ETF_Model_EM.py
+++ pages/ETF_Model_EM.py
@@ -18,6 +18,20 @@ df1 = pd.read_excel(excel_file,
                    header=5,
                    nrows=21)
 
+df1 = df1.rename(columns={'Unnamed: 1' : 'TICKER'})
+df1 = df1.rename(columns={'Unnamed: 2' : 'ETF'})
+df1 = df1.rename(columns={'Unnamed: 3' : 'VALUE'})
+df1 = df1.rename(columns={'Unnamed: 4' : 'QUALITY'})
+df1 = df1.rename(columns={'Unnamed: 5' : 'RISK'})
+df1 = df1.rename(columns={'Unnamed: 6' : 'COMPOSITE VALUE SCORE'})
+df1 = df1.rename(columns={'Unnamed: 7' : 'LIQUIDITY'})
+df1 = df1.rename(columns={'Unnamed: 8' : 'SCORE'})
+df1 = df1.rename(columns={'Unnamed: 9' : 'UPGRADES'})
+df1 = df1.rename(columns={'Unnamed: 10' : 'CURRENCY'})
+df1 = df1.rename(columns={'Unnamed: 11' : 'OVERALL SCORE'})
+df1 = df1.rename(columns={'Unnamed: 12' : 'OVERALL RANK'})
+
+
 st.markdown('### OUTPUT COUNTRY')
 st.dataframe(df1, hide_index=True)
 
