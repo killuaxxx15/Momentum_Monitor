209ccc8e6a1803d181a6279725bacef774a82695
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-03-06 18:29:42 +0800

    Update ETF_Model_EM.py

--- pages/ETF_Model_EM.py
+++ pages/ETF_Model_EM.py
@@ -5,3 +5,18 @@ st.set_page_config(page_title='ETF Model EM',
                    page_icon=':bar_chart:')
 
 st.header('ETF Model EM')
+st.markdown('#### Updated: 06/03/2024')
+
+excel_file = 'ETF_Model_EM.xlsx'
+sheet_name1 = 'OUTPUT_COUNTRY'
+sheet_name2 = 'OUTPUT_PPT'
+
+# TABLE 1
+df1 = pd.read_excel(excel_file,
+                   sheet_name=sheet_name1,
+                   usecols='B:N',
+                   header=5,
+                   nrows=21)
+
+st.markdown('### Table 1: OUTPUT_COUNTRY')
+st.dataframe(df1, hide_index=True)
