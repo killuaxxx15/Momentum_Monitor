c880a7f92bc235a201c2110f047ff3d10ede93b6
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-03-16 09:31:15 +0800

    Update Momentum_Monitor_US_ETF.py

--- pages/Momentum_Monitor_US_ETF.py
+++ pages/Momentum_Monitor_US_ETF.py
@@ -26,9 +26,9 @@ def color_circle(val):
         return '🟡'  # Yellow circle otherwise
 
 def color_circle_1(val):
-    if val >= 20:
+    if val > 16:
         return '🔴'  
-    elif val <= 7:
+    elif val < 8:
         return '🟢'  
     else:
         return '🟡'  
@@ -74,7 +74,7 @@ relative_ranking = df2['Relative ranking']
 df2 = df2.drop(['Relative ranking'], axis=1)
 df2 = df2.drop(['Unnamed: 9'], axis=1)
 df2.insert(2, "Relative Ranking", relative_ranking)
-#df2['Relative Ranking'] = df2['Relative Ranking'].apply(color_circle_1)
+df2['Relative Ranking'] = df2['Relative Ranking'].apply(color_circle_1)
 df2 = df2.style.format({
       'U/D': percent_one_decimal, 
       'Breadth': percent_whole_number, 
