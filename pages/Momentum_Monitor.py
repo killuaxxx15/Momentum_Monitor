212928c172aebf6844c6b8fffd27f54ee7590534
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-03-22 21:50:19 +0800

    Update Momentum_Monitor.py

--- pages/Momentum_Monitor.py
+++ pages/Momentum_Monitor.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(page_title='Global Momentum',
-                   page_icon=':bar_chart:')
-
+st.set_page_config(page_title='Global Momentum', page_icon=':bar_chart:')
 st.header('Global Momentum Dashboard')
 st.markdown('#### Updated: 15/03/2024')
 
 excel_file = 'Global-macro-rankings-final-15032024.xlsx'
 sheet_name = 'Aset class Rankings'
 
+@st.cache
+def load_data(excel_file, sheet_name, usecols, header, nrows):
+    return pd.read_excel(excel_file, sheet_name=sheet_name, usecols=usecols, header=header, nrows=nrows)
 
-# Function to apply color based on cell value
 def color_cells(val):
     color = '#ffcccc' if val == 'CASH' else ('#ccffcc' if val == 'INVESTED' else '')
     return f'background-color: {color}'
@@ -20,7 +20,6 @@ def color_cells_1(val):
     color = '#ffcccc' if val >= 5 else ('#ccffcc' if val <=2 else '#ffffcc')
     return f'background-color: {color}'
 
-# Define the color_circle function
 def color_circle(val):
     if val >= 8:
         return '🔴'  
@@ -47,61 +46,23 @@ def percent_one_decimal(val):
 def percent_whole_number(val):
     return "{:.0f}%".format(val * 100)
 
-
-
-
 # TABLE 1
-df1 = pd.read_excel(excel_file,
-                   sheet_name=sheet_name,
-                   usecols='E:H',
-                   header=2,
-                   nrows=5)
-
+df1 = load_data(excel_file, sheet_name, 'E:H', 2, 5)
 df1 = df1.style.applymap(color_cells, subset=['Above 30 D ', 'Above 60 D', 'Above 200D'])
 st.markdown('### Table 1: Equity Relative to other Asset Classes')
 st.dataframe(df1, hide_index=True)
 
-
-
 # TABLE 2
-df2 = pd.read_excel(excel_file,
-                   sheet_name=sheet_name,
-                   usecols='E:J',
-                   header=13,
-                   nrows=12)
-
-df2 = df2.rename(columns={'Unnamed: 4' : 'ETF'})
-df2 = df2.rename(columns={'Unnamed: 5' : 'Relative Ranking'})
-df2 = df2.rename(columns={'Unnamed: 6' : 'Relative Ranking.1'})
+df2 = load_data(excel_file, sheet_name, 'E:J', 13, 12)
+df2.columns = ['ETF', 'Above 30 D ', 'Above 60 D', 'Above 200D', 'Relative Ranking', 'Relative Ranking.1']
 df2['Relative Ranking.1'] = df2['Relative Ranking.1'].apply(color_circle)
 df2 = df2.style.applymap(color_cells, subset=['Above 30 D ', 'Above 60 D', 'Above 200D'])
 st.markdown('### Table 2: Relative Ranking')
 st.dataframe(df2, hide_index=True)
 
-
-
-
 # TABLE 3
-df3 = pd.read_excel(excel_file,
-                   sheet_name=sheet_name,
-                   usecols='E:P',
-                   header=29,
-                   nrows=10)
-
-df3 = df3.rename(columns={'Unnamed: 5' : 'U/D'})
-df3 = df3.rename(columns={'Unnamed: 6' : 'Breadth'})
-df3 = df3.rename(columns={'Unnamed: 7' : 'Closeness to 52 week'})
-df3 = df3.rename(columns={'Unnamed: 8' : '3 month return'})
-df3 = df3.rename(columns={'Unnamed: 10' : 'U/D.1'})
-df3 = df3.rename(columns={'Unnamed: 11' : 'Breadth.1'})
-df3 = df3.rename(columns={'Unnamed: 12' : 'Closeness to 52 week.1'})
-df3 = df3.rename(columns={'Unnamed: 13' : '3 month return.1'})
-df3 = df3.rename(columns={'Unnamed: 14' : 'Median'})
-df3 = df3.rename(columns={'Unnamed: 15' : 'Relative Ranking'})
-relative_ranking = df3['Relative Ranking']
-df3 = df3.drop(['Relative Ranking'], axis=1)
-df3 = df3.drop(['Unnamed: 9'], axis=1)
-df3.insert(1, "Relative Ranking", relative_ranking)
+df3 = load_data(excel_file, sheet_name, 'E:P', 29, 10)
+df3.columns = ['ETF', 'Relative Ranking', 'U/D', 'Breadth', 'Closeness to 52 week', '3 month return', 'U/D.1', 'Breadth.1', 'Closeness to 52 week.1', '3 month return.1', 'Median']
 df3['Relative Ranking'] = df3['Relative Ranking'].apply(color_circle_1)
 df3 = df3.style.format({
       'U/D': percent_one_decimal, 
@@ -113,56 +74,22 @@ df3 = df3.style.format({
 st.markdown('### Table 3: Equity Ranking: Momentum + Breadth + Upgrades')
 st.dataframe(df3, hide_index=True)
 
-
-
-
 # TABLE 4
-df4 = pd.read_excel(excel_file,
-                   sheet_name=sheet_name,
-                   usecols='E:I',
-                   header=42,
-                   nrows=9)
-
+df4 = load_data(excel_file, sheet_name, 'E:I', 42, 9)
 df4 = df4.style.applymap(color_cells, subset=['Above 30D', 'Above 60 D', 'Above 200D'])
 st.markdown('### Table 4: Equity ETF - MA Signals')
 st.dataframe(df4, hide_index=True)
 
-
-
-
 # TABLE 5
-df5 = pd.read_excel(excel_file,
-                   sheet_name=sheet_name,
-                   usecols='K:M',
-                   header=42,
-                   nrows=10)
-
-df5 = df5.rename(columns={'Unnamed: 10' : 'ETF'})
+df5 = load_data(excel_file, sheet_name, 'K:M', 42, 10)
+df5.columns = ['ETF', 'Upgrades 1 month', 'Downgrades 1 month']
 df5 = df5.style.format({'Upgrades 1 month': percent_one_decimal, 'Downgrades 1 month': percent_one_decimal})
 st.markdown('### Table 5: Equity ETF - Upgrades')
 st.dataframe(df5, hide_index=True)
 
-
-
-
 # TABLE 6
-df6 = pd.read_excel(excel_file,
-                   sheet_name=sheet_name,
-                   usecols='E:F',
-                   header=54,
-                   nrows=7)
-
-df6 = df6.rename(columns={'Table 6 : Long Term forecasts ( above local rates )' : 'ETF'})
-df6 = df6.rename(columns={'Unnamed: 5' : 'Long Term Forecasts'})
+df6 = load_data(excel_file, sheet_name, 'E:F', 54, 7)
+df6.columns = ['ETF', 'Long Term Forecasts']
 df6 = df6.style.format({'Long Term Forecasts': percent_one_decimal})
 st.markdown('### Table 6: Long Term Forecasts (above local rates)')
 st.dataframe(df6, hide_index=True)
-
-
-
-
-
-
-
-
-
