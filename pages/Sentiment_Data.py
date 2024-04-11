a2581bf4463d262d666e3fe15d25109a422b0097
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-04-11 12:10:07 +0800

    Create Sentiment_Data.py

---
+++ pages/Sentiment_Data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+
+# Set Streamlit page configuration
+st.set_page_config(page_title='Sentiment Data', page_icon=':bar_chart:')
+
+# Display header for the dashboard
+st.header('Sentiment Data to be updated')
