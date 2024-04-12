04cbc5ab2ad77450b1514302d9293bca0151e994
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-04-13 07:19:06 +0800

    Update Sentiment_Data.py

--- pages/Sentiment_Data.py
+++ pages/Sentiment_Data.py
@@ -35,27 +35,9 @@ st.dataframe(df2, hide_index=True)
 
 
 st.header('Bull/Bear Ratios')
-# Create an expander for the figures
-with st.expander("See Figures"):
-    st.image('pic1.png', caption='Figure 1', use_column_width=True)
-    st.image('pic2.png', caption='Figure 2', use_column_width=True)
-    st.image('pic3.png', caption='Figure 3', use_column_width=True)
-    st.image('pic4.png', caption='Figure 4', use_column_width=True)
-    st.image('pic5.png', caption='Figure 5', use_column_width=True)
-    st.image('pic6.png', caption='Figure 6', use_column_width=True)
-    st.image('pic7.png', caption='Figure 7', use_column_width=True)
-    st.image('pic8.png', caption='Figure 8', use_column_width=True)
-    st.image('pic9.png', caption='Figure 9', use_column_width=True)
-    st.image('pic10.png', caption='Figure 10', use_column_width=True)
-    st.image('pic11.png', caption='Figure 11', use_column_width=True)
-    st.image('pic12.png', caption='Figure 12', use_column_width=True)
-    st.image('pic13.png', caption='Figure 13', use_column_width=True)
+st.image('pic1.png', caption='Figure 1', use_column_width=True)
 
 
 st.header('Put/Call & Vix')
-with st.expander("See Figures"):
-    st.image('fig1.png', caption='Figure 1', use_column_width=True)
-    st.image('fig2.png', caption='Figure 2', use_column_width=True)
-    st.image('fig3.png', caption='Figure 3', use_column_width=True)
-    st.image('fig4.png', caption='Figure 4', use_column_width=True)
-    st.image('fig5.png', caption='Figure 5', use_column_width=True)
+st.image('fig1.png', caption='Figure 1', use_column_width=True)
+
