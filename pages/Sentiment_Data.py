220ddf640e5ac598b99659c01b5ce3b7dc956825
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-04-12 11:17:59 +0800

    Update Sentiment_Data.py

--- pages/Sentiment_Data.py
+++ pages/Sentiment_Data.py
@@ -18,4 +18,5 @@ def load_excel_data(file_name, sheet, use_columns, header_row, num_rows):
 
 # TABLE 1
 df1 = load_excel_data(excel_file, sheet_name, 'F:K', 10, 5)
+df1 = df1.fillna('')
 st.dataframe(df1, hide_index=True)
