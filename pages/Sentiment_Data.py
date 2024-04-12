13edbbb2fdf5130db918a40296873fde8309eeb2
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-04-12 12:04:45 +0800

    Update Sentiment_Data.py

--- pages/Sentiment_Data.py
+++ pages/Sentiment_Data.py
@@ -19,6 +19,8 @@ def load_excel_data(file_name, sheet, use_columns, header_row, num_rows):
 # TABLE 1
 df1 = load_excel_data(excel_file, sheet_name, 'F:K', 10, 5)
 df1 = df1.fillna('')
+# Format specific cells as percentages
+df1.iloc[2:4, 2] = df1.iloc[2:4, 2].apply(lambda x: f'{x:.2%}')
 st.dataframe(df1, hide_index=True)
 st.markdown(' ### ')
 
