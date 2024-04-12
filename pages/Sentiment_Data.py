c444ed85f5d2ba4a21dd96334847926f2441cf0f
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-04-12 11:15:41 +0800

    Update Sentiment_Data.py

--- pages/Sentiment_Data.py
+++ pages/Sentiment_Data.py
@@ -6,3 +6,16 @@ st.set_page_config(page_title='Sentiment Data', page_icon=':bar_chart:')
 
 # Display header for the dashboard
 st.header('Sentiment Data to be updated')
+
+# Define Excel file and sheet name variables
+excel_file = 'Sentimnet data.xlsx'
+sheet_name = 'Sheet1'
+
+# Cache data loading function for better performance
+@st.cache
+def load_excel_data(file_name, sheet, use_columns, header_row, num_rows):
+    return pd.read_excel(file_name, sheet_name=sheet, usecols=use_columns, header=header_row, nrows=num_rows)
+
+# TABLE 1
+df1 = load_excel_data(excel_file, sheet_name, 'F:K', 10, 5)
+st.dataframe(df1, hide_index=True)
