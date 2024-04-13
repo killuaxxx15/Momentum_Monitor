581d402dc1d9f9f503c59128bb1b4ca2f3ce6709
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-04-13 08:25:01 +0800

    Update Momentum_Monitor.py

--- pages/Momentum_Monitor.py
+++ pages/Momentum_Monitor.py
@@ -62,14 +62,14 @@ def percent_whole_number(val):
 
 
 # TABLE 1
-df1 = load_excel_data(excel_file, sheet_name, 'E:H', 2, 5)
+df1 = load_excel_data(excel_file, sheet_name, 'E:H', 2, 6)
 df1 = df1.style.applymap(color_cells, subset=['Above 30 D ', 'Above 60 D', 'Above 200D'])
 st.markdown('### Table 1: Equity Relative to other Asset Classes')
 st.dataframe(df1, hide_index=True)
 
 
 # TABLE 2
-df2 = load_excel_data(excel_file, sheet_name, 'E:J', 13, 12)
+df2 = load_excel_data(excel_file, sheet_name, 'E:J', 13, 13)
 df2 = df2.rename(columns={'Unnamed: 4' : 'ETF'})
 df2 = df2.rename(columns={'Unnamed: 5' : 'Relative Ranking'})
 df2 = df2.rename(columns={'Unnamed: 6' : 'Relative Ranking.1'})
@@ -80,7 +80,7 @@ st.dataframe(df2, hide_index=True)
 
 
 # TABLE 3
-df3 = load_excel_data(excel_file, sheet_name, 'E:P', 29, 10)
+df3 = load_excel_data(excel_file, sheet_name, 'E:P', 30, 10)
 df3 = df3.rename(columns={'Unnamed: 5' : 'U/D'})
 df3 = df3.rename(columns={'Unnamed: 6' : 'Breadth'})
 df3 = df3.rename(columns={'Unnamed: 7' : 'Closeness to 52 week'})
