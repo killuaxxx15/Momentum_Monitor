69478d26a5a511e782ea30b1492fbdd5e51fcad8
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-04-19 18:51:50 +0800

    Update ETF_Model_EM.py

--- pages/ETF_Model_EM.py
+++ pages/ETF_Model_EM.py
@@ -11,7 +11,7 @@ st.header('ETF Model EM')
 st.markdown('#### Updated: 12/04/2024')
 
 # Define Excel file and sheet name variables
-excel_file = 'ETF_Model_EM_12_04_2024.xlsx'
+excel_file = 'ETF_Model_EM_19_04_2024.xlsx'
 sheet_name1 = 'OUTPUT_COUNTRY'
 sheet_name2 = 'OUTPUT_PPT'
 
