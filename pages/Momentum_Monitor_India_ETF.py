933fcf90d312ff76a4fad0301c68ed0acd30078d
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-04-20 08:36:44 +0800

    Update Momentum_Monitor_India_ETF.py

--- pages/Momentum_Monitor_India_ETF.py
+++ pages/Momentum_Monitor_India_ETF.py
@@ -11,7 +11,7 @@ st.header('Momentum Monitor India ETF (lower is better)')
 st.markdown('#### Updated: 12/04/2024')
 
 # Define Excel file and sheet name variables
-excel_file = 'INDIA_ETF_MOMENTUM_RANKINGS.xlsx'
+excel_file = 'INDIA_ETF_MOMENTUM_RANKINGS_19_04_2024.xlsx'
 sheet_name = 'Aset class Rankings'
 
 # Cache data loading function for better performance
@@ -30,10 +30,10 @@ def color_cells_1(val):
     return f'background-color: {color}'
 
 # Define function to display colored circle based on cell value
-def color_circle(val, lowest_5, highest_5):
-    if val in highest_5:
+def color_circle(val):
+    if val >= 10:
         return '🔴'  
-    elif val in lowest_5:
+    elif val < 4:
         return '🟢' 
     else:
         return '🟡'
@@ -62,28 +62,19 @@ def percent_whole_number(val):
 
 
 # TABLE 1
-df1 = load_excel_data(excel_file, sheet_name, 'D:K', 5, 15)
+df1 = load_excel_data(excel_file, sheet_name, 'D:J', 5, 14)
 df1 = df1.rename(columns={'Unnamed: 3' : 'TICKER'})
 df1 = df1.rename(columns={'Unnamed: 4' : 'ETF'})
-df1 = df1.rename(columns={'Above 30 D ' : 'Current Ranking.1'})
-df1 = df1.rename(columns={'Above 60 D' : 'Above 30 D '})
-df1 = df1.rename(columns={'Above 200D' : 'Above 60 D'})
-df1 = df1.rename(columns={'Unnamed: 10' : 'Above 200D'})
-ranking_1 = df1['Current Ranking.1']
-# Sort the 'Relative Ranking' series in ascending order
-sorted_ranking_1 = ranking_1.sort_values()
-# Determine the thresholds for the lowest 5 and highest 5 values
-lowest_5 = sorted_ranking_1.head(5).values
-highest_5 = sorted_ranking_1.tail(5).values
-# Apply the color_circle_1 function to each value in the 'Current Ranking.1' column
-df1['Current Ranking.1'] = ranking_1.apply(color_circle, args=(lowest_5, highest_5))
+df1 = df1.rename(columns={'Unnamed: 5' : 'Relative Ranking'})
+df1 = df1.rename(columns={'Unnamed: 6' : 'Relative Ranking.1'})
+df1['Relative Ranking.1'] = df1['Relative Ranking.1'].apply(color_circle)
 df1 = df1.style.applymap(color_cells, subset=['Above 30 D ', 'Above 60 D', 'Above 200D'])
 st.markdown('### Relative Ranking')
 st.dataframe(df1, hide_index=True)
 
 
 # TABLE 2
-df2 = load_excel_data(excel_file, sheet_name, 'D:P', 23, 15)
+df2 = load_excel_data(excel_file, sheet_name, 'D:P', 23, 14)
 df2 = df2.rename(columns={'Unnamed: 3' : 'TICKER'})
 df2 = df2.rename(columns={'Unnamed: 4' : 'ETF'})
 df2 = df2.rename(columns={'Clsoeness to 52 week' : 'Closeness to 52 week.1'})
@@ -92,18 +83,7 @@ relative_ranking = df2['Relative ranking']
 df2 = df2.drop(['Relative ranking'], axis=1)
 df2 = df2.drop(['Unnamed: 9'], axis=1)
 df2.insert(2, "Relative Ranking", relative_ranking)
-#df2['Relative Ranking'] = df2['Relative Ranking'].apply(color_circle_1)
-
-# Sort the 'Relative Ranking' series in ascending order
-sorted_relative_ranking = relative_ranking.sort_values()
-
-# Determine the thresholds for the lowest 10 and highest 10 values
-lowest_5 = sorted_relative_ranking.head(5).values
-highest_5 = sorted_relative_ranking.tail(5).values
-
-# Apply the color_circle_1 function to each value in the 'Relative Ranking' column
-df2['Relative Ranking'] = relative_ranking.apply(color_circle, args=(lowest_5, highest_5))
-
+df2['Relative Ranking'] = df2['Relative Ranking'].apply(color_circle_1)
 df2 = df2.style.format({
       'U/D': percent_one_decimal, 
       'Breadth': percent_whole_number, 
@@ -116,7 +96,7 @@ st.dataframe(df2, hide_index=True)
 
 
 # TABLE 3
-df3 = load_excel_data(excel_file, sheet_name, 'D:I', 42, 15)
+df3 = load_excel_data(excel_file, sheet_name, 'D:I', 42, 14)
 df3 = df3.drop(['Unnamed: 5'], axis=1)
 df3 = df3.style.applymap(color_cells)
 st.markdown('### Equity ETF - MA Signals')
@@ -124,7 +104,7 @@ st.dataframe(df3, hide_index=True)
 
 
 # TABLE 4
-df4 = load_excel_data(excel_file, sheet_name, 'K:N', 42, 15)
+df4 = load_excel_data(excel_file, sheet_name, 'K:N', 42, 14)
 df4 = df4.rename(columns={'TICKER.1' : 'TICKER'})
 df4 = df4.rename(columns={'ETF.1' : 'ETF'})
 df4 = df4.style.format({'Upgrades 1 month': percent_one_decimal, 'Downgrades 1 month': percent_one_decimal})
