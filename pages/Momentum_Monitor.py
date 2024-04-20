694a42264ea41e0e41078600bab302e48b07268e
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-04-20 08:31:09 +0800

    Update Momentum_Monitor.py

--- pages/Momentum_Monitor.py
+++ pages/Momentum_Monitor.py
@@ -11,7 +11,7 @@ st.header('Global Momentum Dashboard')
 st.markdown('#### Updated: 12/04/2024')
 
 # Define Excel file and sheet name variables
-excel_file = 'Global_macro_rankings_final_12042024.xlsx'
+excel_file = 'Global_macro_rankings_final_19_04_2024.xlsx'
 sheet_name = 'Aset class Rankings'
 
 # Cache data loading function for better performance
@@ -30,13 +30,13 @@ def color_cells_1(val):
     return f'background-color: {color}'
 
 # Define function to display colored circle based on cell value
-def color_circle(val, lowest_4, highest_4):
-    if val in highest_4:
+def color_circle(val):
+    if val >= 10:
         return '🔴'  
-    elif val in lowest_4:
-        return '🟢' 
+    elif val <= 4:
+        return '🟢'  
     else:
-        return '🟡'
+        return '🟡'  
 
 # Function to apply background color based on cell value ranges
 def color_cells_2(val):
@@ -44,13 +44,13 @@ def color_cells_2(val):
     return f'background-color: {color}'
 
 # Define function to display colored circle based on cell value
-def color_circle_1(val, lowest_3, highest_3):
-    if val in highest_3:
+def color_circle_1(val):
+    if val >= 3:
         return '🔴'  
-    elif val in lowest_3:
-        return '🟢' 
+    elif val < 0:
+        return '🟢'  
     else:
-        return '🟡'
+        return '🟡' 
 
 # Format percentage with one decimal place
 def percent_one_decimal(val):
@@ -69,24 +69,18 @@ st.dataframe(df1, hide_index=True)
 
 
 # TABLE 2
-df2 = load_excel_data(excel_file, sheet_name, 'E:K', 13, 14)
+df2 = load_excel_data(excel_file, sheet_name, 'E:J', 13, 13)
 df2 = df2.rename(columns={'Unnamed: 4' : 'ETF'})
-df2 = df2.rename(columns={'Unnamed: 7' : 'Current Ranking.1'})
-ranking_1 = df2['Current Ranking.1']
-# Sort the 'Current Ranking.1' series in ascending order
-sorted_ranking_1 = ranking_1.sort_values()
-# Determine the thresholds for the lowest 4 and highest 4 values
-lowest_4 = sorted_ranking_1.head(4).values
-highest_4 = sorted_ranking_1.tail(4).values
-# Apply the color_circle function to each value in the 'Current Ranking.1' column
-df2['Current Ranking.1'] = ranking_1.apply(color_circle, args=(lowest_4, highest_4))
+df2 = df2.rename(columns={'Unnamed: 5' : 'Relative Ranking'})
+df2 = df2.rename(columns={'Unnamed: 6' : 'Relative Ranking.1'})
+df2['Relative Ranking.1'] = df2['Relative Ranking.1'].apply(color_circle)
 df2 = df2.style.applymap(color_cells, subset=['Above 30 D ', 'Above 60 D', 'Above 200D'])
 st.markdown('### Table 2: Relative Ranking')
 st.dataframe(df2, hide_index=True)
 
 
 # TABLE 3
-df3 = load_excel_data(excel_file, sheet_name, 'E:P', 31, 10)
+df3 = load_excel_data(excel_file, sheet_name, 'E:P', 30, 10)
 df3 = df3.rename(columns={'Unnamed: 5' : 'U/D'})
 df3 = df3.rename(columns={'Unnamed: 6' : 'Breadth'})
 df3 = df3.rename(columns={'Unnamed: 7' : 'Closeness to 52 week'})
@@ -101,13 +95,7 @@ relative_ranking = df3['Relative Ranking']
 df3 = df3.drop(['Relative Ranking'], axis=1)
 df3 = df3.drop(['Unnamed: 9'], axis=1)
 df3.insert(1, "Relative Ranking", relative_ranking)
-# Sort the 'Relative Ranking' series in ascending order
-sorted_relative_ranking = relative_ranking.sort_values()
-# Determine the thresholds for the lowest 10 and highest 10 values
-lowest_3 = sorted_relative_ranking.head(3).values
-highest_3 = sorted_relative_ranking.tail(3).values
-# Apply the color_circle_1 function to each value in the 'Relative Ranking' column
-df3['Relative Ranking'] = relative_ranking.apply(color_circle_1, args=(lowest_3, highest_3))
+df3['Relative Ranking'] = df3['Relative Ranking'].apply(color_circle_1)
 df3 = df3.style.format({
       'U/D': percent_one_decimal, 
       'Breadth': percent_whole_number, 
@@ -120,7 +108,7 @@ st.dataframe(df3, hide_index=True)
 
 
 # TABLE 4
-df4 = load_excel_data(excel_file, sheet_name, 'E:I', 44, 9)
+df4 = load_excel_data(excel_file, sheet_name, 'E:I', 43, 9)
 df4 = df4.style.applymap(color_cells, subset=['Above 30D', 'Above 60 D', 'Above 200D'])
 st.markdown('### Table 4: Equity ETF - MA Signals')
 st.dataframe(df4, hide_index=True)
@@ -132,7 +120,7 @@ df5 = pd.read_excel(excel_file,
                    usecols='K:M',
                    header=42,
                    nrows=10)
-df5 = load_excel_data(excel_file, sheet_name, 'K:M', 44, 10)
+df5 = load_excel_data(excel_file, sheet_name, 'K:M', 43, 10)
 df5 = df5.rename(columns={'Unnamed: 10' : 'ETF'})
 df5 = df5.style.format({'Upgrades 1 month': percent_one_decimal, 'Downgrades 1 month': percent_one_decimal})
 st.markdown('### Table 5: Equity ETF - Upgrades')
@@ -140,7 +128,7 @@ st.dataframe(df5, hide_index=True)
 
 
 # TABLE 6
-df6 = load_excel_data(excel_file, sheet_name, 'E:F', 56, 7)
+df6 = load_excel_data(excel_file, sheet_name, 'E:F', 55, 7)
 df6 = df6.rename(columns={'Table 6 : Long Term forecasts ( above local rates )' : 'ETF'})
 df6 = df6.rename(columns={'Unnamed: 5' : 'Long Term Forecasts'})
 df6 = df6.style.format({'Long Term Forecasts': percent_one_decimal})
