13891d7eba723171da925bbd696c4748e940937f
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-05-11 13:42:29 +0800

    Update Sentiment_Data.py

--- pages/Sentiment_Data.py
+++ pages/Sentiment_Data.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title='Sentiment Data', page_icon=':bar_chart:')
 st.header('Sentiment Data')
 
 # Display the last update date
-st.markdown('#### Updated: 03/05/2024')
+st.markdown('#### Updated: 10/05/2024')
 
 # Define Excel file and sheet name variables
 excel_file = 'Sentiment_Data_10_05_2024.xlsx'
