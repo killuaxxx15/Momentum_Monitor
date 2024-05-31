89ccafe37fc24b5bd706d52befb6c11516df219b
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-05-31 10:08:46 +0800

    Create Commodities.py

---
+++ pages/Commodities.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+
+# Set Streamlit page configuration
+st.set_page_config(page_title='Commodities', page_icon=':bar_chart:')
+
+# Display header for the dashboard
+st.header('Commodities')
