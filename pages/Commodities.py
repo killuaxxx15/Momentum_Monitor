d855b513131207dc00e850794b8ffa0163ff6249
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-06-10 14:59:54 +0800

    Update Commodities.py

--- pages/Commodities.py
+++ pages/Commodities.py
@@ -6,3 +6,31 @@ st.set_page_config(page_title='Commodities', page_icon=':bar_chart:')
 
 # Display header for the dashboard
 st.header('Commodities')
+
+# Display the last update date
+st.markdown('#### Updated: 07/06/2024')
+
+# Define Excel file and sheet name variables
+excel_file = 'COMMODITIES_MOMENTUM_RANKINGS_10_06_2024.xlsx'
+sheet_name = 'Aset class Rankings'
+
+# Cache data loading function for better performance
+@st.cache
+def load_excel_data(file_name, sheet, use_columns, header_row, num_rows):
+    return pd.read_excel(file_name, sheet_name=sheet, usecols=use_columns, header=header_row, nrows=num_rows)
+
+# Function to apply background color based on cell value
+def color_cells(val):
+    color = '#ffcccc' if val == 'CASH' else ('#ccffcc' if val == 'INVESTED' else '')
+    return f'background-color: {color}'
+
+
+# TABLE 1
+df1 = load_excel_data(excel_file, sheet_name, 'D:K', 5, 14)
+df1 = df1.style.applymap(color_cells, subset=['Above 30 D ', 'Above 60 D', 'Above 200D'])
+st.markdown('### Relative Ranking')
+st.dataframe(df1, hide_index=True)
+
+
+
+
