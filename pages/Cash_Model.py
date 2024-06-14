dd17e999ea34f4b9d9210c3b0e82ce7770638331
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-06-14 10:53:35 +0800

    Update Cash_Model.py

--- pages/Cash_Model.py
+++ pages/Cash_Model.py
@@ -8,10 +8,10 @@ st.set_page_config(page_title='Cash Model', page_icon=':bar_chart:')
 st.header('Cash Model')
 
 # Display the last update date
-st.markdown('#### Updated: 12/06/2024')
+st.markdown('#### Updated: 14/06/2024')
 
 # Define Excel file and sheet name variables
-excel_file = 'Cash_model_10_06_2024.xlsx'
+excel_file = 'Cash_model_14_06_2024.xlsx'
 sheet_name = 'Sheet1'
 
 
@@ -22,11 +22,11 @@ def load_excel_data(file_name, sheet, use_columns, header_row, num_rows):
 
 
 # TABLE 1
-df1 = load_excel_data(excel_file, sheet_name, 'C:H', 7, 6)
+df1 = load_excel_data(excel_file, sheet_name, 'C:H', 7, 9)
 st.markdown('### Table 1')
 st.dataframe(df1, hide_index=True)
 
 # TABLE 2
-df2 = load_excel_data(excel_file, sheet_name, 'C:E', 16, 1)
+df2 = load_excel_data(excel_file, sheet_name, 'C:E', 19, 1)
 st.markdown('### Table 2')
 st.dataframe(df2, hide_index=True)
