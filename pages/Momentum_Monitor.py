8f0c2f7947a2d15440a0484b5efddfb73c23ab4c
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-06-16 09:26:30 +0800

    Update Momentum_Monitor.py

--- pages/Momentum_Monitor.py
+++ pages/Momentum_Monitor.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title='Global Momentum', page_icon=':bar_chart:')
 st.header('Global Momentum Dashboard')
 
 # Display the last update date
-st.markdown('#### Updated: 07/06/2024')
+st.markdown('#### Updated: 07/06/2024 (waiting for new data)')
 
 # Define Excel file and sheet name variables
 excel_file = 'Global_macro_rankings_final_07_06_2024.xlsx'
