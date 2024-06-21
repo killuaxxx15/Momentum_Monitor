d0528223a0ce95fe1e50300330208f3c6b240cb7
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-06-21 20:39:55 +0800

    Update Momentum_Monitor_US_ETF.py

--- pages/Momentum_Monitor_US_ETF.py
+++ pages/Momentum_Monitor_US_ETF.py
@@ -8,10 +8,10 @@ st.set_page_config(page_title='US ETF Momentum Monitor', page_icon=':bar_chart:'
 st.header('Momentum Monitor US ETF (lower is better)')
 
 # Display the last update date
-st.markdown('#### Updated: 14/06/2024')
+st.markdown('#### Updated: 21/06/2024')
 
 # Define Excel file and sheet name variables
-excel_file = 'US_ETF_MOMENTUM_RANKINGS_14_06_2024.xlsx'
+excel_file = 'US_ETF_MOMENTUM_RANKINGS_21_06_2024.xlsx'
 sheet_name = 'Aset class Rankings'
 
 # Cache data loading function for better performance
