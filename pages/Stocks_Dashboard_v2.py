1f4f870a23ca8e348b91eccb9e86e8a696c79220
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-07-11 10:27:23 +0800

    Update Stocks_Dashboard_v2.py

--- pages/Stocks_Dashboard_v2.py
+++ pages/Stocks_Dashboard_v2.py
@@ -13,10 +13,7 @@ import time
 ##################################################################
 
 st.set_page_config(page_title="Stocks Dashboard", layout="wide")
-# Read and apply custom CSS
-with open("styles.html", "r") as f:
-    styles = f.read()
-st.markdown(f"<style>{styles}</style>", unsafe_allow_html=True)
+st.html("styles.html")
 pio.templates.default = "plotly_white"
 
 # from itertools.batched, used to produce rows of columns
