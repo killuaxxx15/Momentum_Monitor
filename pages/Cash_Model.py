b749bec332b2f57a3f67396765e5e64806190d17
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-08-03 12:12:00 +0800

    Update Cash_Model.py

--- pages/Cash_Model.py
+++ pages/Cash_Model.py
@@ -8,10 +8,10 @@ st.set_page_config(page_title='Cash Model', page_icon=':bar_chart:')
 st.header('Cash Model')
 
 # Display the last update date
-st.markdown('#### Updated: 26/07/2024')
+st.markdown('#### Updated: 02/08/2024')
 
 # Define Excel file and sheet name variables
-excel_file = 'Cash_model_26_07_2024.xlsx'
+excel_file = 'Cash_model_02_08_2024.xlsx'
 sheet_name = 'Sheet1'
 
 
