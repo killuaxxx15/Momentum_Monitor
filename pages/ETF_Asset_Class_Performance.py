2d52a67961331e2f90501d27cdcdee8467e89c10
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-08-07 13:00:57 +0800

    Update ETF_Asset_Class_Performance.py

--- pages/ETF_Asset_Class_Performance.py
+++ pages/ETF_Asset_Class_Performance.py
@@ -108,7 +108,8 @@ def create_etf_table(tickers):
     return df.style.format(format_dict).map(color_scale, subset=numeric_columns)
 
 # Main Streamlit app
-st.header('ETF Asset Class Performance YTD, MTD, and This Week (Currently Editing)')
+st.header('Currently Editing')
+st.header('ETF Asset Class Performance YTD, MTD, and This Week')
 
 st.subheader('US Related')
 us_related_etf_table = create_etf_table(us_related_etf_tickers)
