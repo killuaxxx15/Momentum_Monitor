272e2483024d010d23cc42d214645f96fc4a8501
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-08-21 15:23:59 +0800

    Update ETF_Dashboard.py

--- pages/ETF_Dashboard.py
+++ pages/ETF_Dashboard.py
@@ -19,7 +19,7 @@ etf_tickers = [
     "KIE", "TIP", "SHY", "DBA", "UNG", "DBC", "KSA", "USO", "ILF", "UUP", "WEAT",
     "VNM", "EWZ", "^VIX", "BNO", "CBON", "EMCB", "LEMB", "VWO", "EWI",
     "^MOVE", "VGK", "XWD.TO", "ITB", "IYT", "IYJ", "QUAL", "IVW", "IAI", "IGE", "IHI",
-    "IYH", "IYZ", "^RUT", "EMXC", "PRN", "XLI", "FANG.AX", "AIRR", "SPMO", "ITA"
+    "IYH", "IYZ", "^RUT", "EMXC", "PRN", "XLI", "FANG.AX", "AIRR", "SPMO", "ITA", "ACWX"
 ]
 
 @st.cache_data
