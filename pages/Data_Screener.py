496ad83789dcdc82dec13ddc44d39e81de21b74f
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-08-23 11:16:29 +0800

    Update Data_Screener.py

--- pages/Data_Screener.py
+++ pages/Data_Screener.py
@@ -6,5 +6,8 @@ st.set_page_config(page_title='Data Screener', page_icon=':bar_chart:')
 # Display header for the dashboard
 st.header('Data Screener')
 
-# Display the last update date
-st.markdown('Link: ')
+# Define the URL
+url = "https://data-screener.streamlit.app"
+
+# Create a clickable link
+st.markdown(f'Link: [Data Screener]({url})')
