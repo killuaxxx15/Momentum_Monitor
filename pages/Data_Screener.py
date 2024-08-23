262ceb8daaa2b777878b7812d9d2eb737429596d
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-08-23 11:11:10 +0800

    Create Data_Screener.py

---
+++ pages/Data_Screener.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Set Streamlit page configuration
+st.set_page_config(page_title='Data Screener', page_icon=':bar_chart:')
+
+# Display header for the dashboard
+st.header('Data Screener')
+
+# Display the last update date
+st.markdown('Link: ')
