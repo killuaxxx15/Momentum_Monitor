67100fa128bc65237d453436874f378f3e4a5bd0
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-08-25 10:44:41 +0800

    Update Momentum_Monitor_India_ETF.py

--- pages/Momentum_Monitor_India_ETF.py
+++ pages/Momentum_Monitor_India_ETF.py
@@ -8,10 +8,10 @@ st.set_page_config(page_title='India ETF Momentum', page_icon=':bar_chart:')
 st.header('Momentum Monitor India ETF (lower is better)')
 
 # Display the last update date
-st.markdown('#### Updated: 19/08/2024')
+st.markdown('#### Updated: 24/08/2024')
 
 # Define Excel file and sheet name variables
-excel_file = 'INDIA_ETF_MOMENTUM_RANKINGS_19_08_2024.xlsx'
+excel_file = 'INDIA_ETF_MOMENTUM_RANKINGS_24_08_2024.xlsx'
 sheet_name = 'Aset class Rankings'
 
 # Cache data loading function for better performance
