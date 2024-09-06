c5952409e16eefa66df004be1efa56bc5e3c4786
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-09-06 18:21:20 +0800

    Update Sentiment_Data.py

--- pages/Sentiment_Data.py
+++ pages/Sentiment_Data.py
@@ -8,10 +8,10 @@ st.set_page_config(page_title='Sentiment Data', page_icon=':bar_chart:')
 st.header('Sentiment Data')
 
 # Display the last update date
-st.markdown('#### Updated: 30/08/2024')
+st.markdown('#### Updated: 06/09/2024')
 
 # Define Excel file and sheet name variables
-excel_file = 'Sentiment_Data_30_08_2024.xlsx'
+excel_file = 'Sentiment_Data_06_09_2024.xlsx'
 sheet_name = 'Sheet1'
 
 # Cache data loading function for better performance
@@ -36,10 +36,10 @@ df2 = df2.fillna('')
 st.dataframe(df2, hide_index=True)
 
 st.header('Bull/Bear Ratios')
-st.image('fig1_30_08_2024.png', caption='Figure 1', use_column_width=True)
+st.image('fig1_06_09_2024.png', caption='Figure 1', use_column_width=True)
 
 st.header('Put/Call & Vix')
-st.image('fig2_30_08_2024.png', caption='Figure 2', use_column_width=True)
+st.image('fig2_06_09_2024.png', caption='Figure 2', use_column_width=True)
 
 
 
