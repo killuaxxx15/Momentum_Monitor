37a039c64f7a8681fd4c9fcde3e5047305a130c8
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-09-24 08:37:25 +0800

    Update ETF_Dashboard.py

--- pages/ETF_Dashboard.py
+++ pages/ETF_Dashboard.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title='ETF Dashboard', page_icon=':bar_chart:', layout="
 
 # List of ETF tickers
 etf_tickers = [
-    "COPX", "TAN", "SMH", "EZA", "EPOL", "EWY", "SLV", "EWG", "EWT", "KWEB",
+    "GDX", "COPX", "TAN", "SMH", "EZA", "EPOL", "EWY", "SLV", "EWG", "EWT", "KWEB",
     "PPLT", "FXI", "XLB", "ICLN", "TUR", "MCHI", "EWQ", "ECH", "XME", "CQQQ", "IEV",
     "LIT", "EWL", "GREK", "XLRE", "EPU", "XAR", "EWA", "DEM", "XSOE", "EWJ", "EEM",
     "PCY", "GLD", "EWU", "GNR", "HEDJ", "EWC", "QQQ", "ASHR", "URTH", "EPHE", "RSP",
@@ -110,7 +110,7 @@ def create_relative_performance_chart(etf_data1, etf_data2, etf_name1, etf_name2
     ax.legend()
     return fig
 
-@st.cache_data
+#@st.cache_data
 def get_ETF_info(ticker):
     ETF = yf.Ticker(ticker)
     info = ETF.info
