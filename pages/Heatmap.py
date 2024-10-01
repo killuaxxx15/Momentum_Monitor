c6c636c71c630c96cb39f8cf92e51962f55ebfdc
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-10-01 19:42:54 +0800

    Update Heatmap.py

--- pages/Heatmap.py
+++ pages/Heatmap.py
@@ -149,7 +149,7 @@ def main():
     st.markdown("### To Be Updated")
 
     # Add date input widget
-    end_date = st.date_input("Select End Date", value=pd.to_datetime("2024-09-22"))
+    end_date = st.date_input("Select End Date", value=pd.to_datetime("2024-09-29"))
     end_date = end_date.strftime("%Y-%m-%d")  # Convert to string format
 
     # Add frequency selection widget
