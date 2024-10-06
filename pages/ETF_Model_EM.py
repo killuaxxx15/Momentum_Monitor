58cdda2feaaed9ddc08e6b600662fc3ed68f86e2
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-10-06 09:03:59 +0800

    Update ETF_Model_EM.py

--- pages/ETF_Model_EM.py
+++ pages/ETF_Model_EM.py
@@ -8,10 +8,10 @@ st.set_page_config(page_title='ETF Model EM', page_icon=':bar_chart:')
 st.header('ETF Model EM')
 
 # Display the last update date
-st.markdown('#### Updated: 27/09/2024')
+st.markdown('#### Updated: 05/10/2024')
 
 # Define Excel file and sheet name variables
-excel_file = 'ETF_Model_EM_27_09_2024.xlsx'
+excel_file = 'ETF_Model_EM_05_10_2024.xlsx'
 sheet_name1 = 'OUTPUT_COUNTRY'
 sheet_name2 = 'OUTPUT_PPT'
 
