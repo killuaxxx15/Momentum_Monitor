b71fbe8973a2ed2827694463d251e50d06497f3c
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2024-10-11 09:37:23 +0800

    Update Momentum_Monitor.py

--- pages/Momentum_Monitor.py
+++ pages/Momentum_Monitor.py
@@ -8,14 +8,14 @@ st.set_page_config(page_title='Global Momentum', page_icon=':bar_chart:')
 st.header('Global Momentum Dashboard')
 
 # Display the last update date
-st.markdown('#### Updated: 05/10/2024')
+st.markdown('#### Updated: 10/10/2024')
 
 # Define Excel file and sheet name variables
-excel_file = 'Global_macro_rankings_final_05_10_2024.xlsx'
+excel_file = 'Global_macro_rankings_final_10_10_2024.xlsx'
 sheet_name = 'Aset class Rankings'
 
 # Cache data loading function for better performance
-@st.cache
+@st.cache_data
 def load_excel_data(file_name, sheet, use_columns, header_row, num_rows):
     return pd.read_excel(file_name, sheet_name=sheet, usecols=use_columns, header=header_row, nrows=num_rows)
 
@@ -62,14 +62,14 @@ def percent_whole_number(val):
 
 
 # TABLE 1
-df1 = load_excel_data(excel_file, sheet_name, 'E:H', 2, 6)
+df1 = load_excel_data(excel_file, sheet_name, 'E:H', 2, 9)
 df1 = df1.style.applymap(color_cells, subset=['Above 30 D ', 'Above 60 D', 'Above 200D'])
 st.markdown('### Table 1: Equity Relative to other Asset Classes')
 st.dataframe(df1, hide_index=True)
 
 
 # TABLE 2
-df2 = load_excel_data(excel_file, sheet_name, 'E:K', 13, 14)
+df2 = load_excel_data(excel_file, sheet_name, 'E:K', 16, 14)
 df2 = df2.rename(columns={'Unnamed: 4' : 'ETF'})
 df2 = df2.rename(columns={'Unnamed: 7' : 'Current Ranking.1'})
 ranking_1 = df2['Current Ranking.1']
@@ -86,7 +86,7 @@ st.dataframe(df2, hide_index=True)
 
 
 # TABLE 3
-df3 = load_excel_data(excel_file, sheet_name, 'E:P', 31, 10)
+df3 = load_excel_data(excel_file, sheet_name, 'E:P', 34, 10)
 df3 = df3.rename(columns={'Unnamed: 5' : 'U/D'})
 df3 = df3.rename(columns={'Unnamed: 6' : 'Breadth'})
 df3 = df3.rename(columns={'Unnamed: 7' : 'Closeness to 52 week'})
@@ -120,7 +120,7 @@ st.dataframe(df3, hide_index=True)
 
 
 # TABLE 4
-df4 = load_excel_data(excel_file, sheet_name, 'E:I', 44, 9)
+df4 = load_excel_data(excel_file, sheet_name, 'E:I', 47, 9)
 df4 = df4.style.applymap(color_cells, subset=['Above 30D', 'Above 60 D', 'Above 200D'])
 st.markdown('### Table 4: Equity ETF - MA Signals')
 st.dataframe(df4, hide_index=True)
@@ -132,7 +132,7 @@ df5 = pd.read_excel(excel_file,
                    usecols='K:M',
                    header=42,
                    nrows=10)
-df5 = load_excel_data(excel_file, sheet_name, 'K:M', 44, 10)
+df5 = load_excel_data(excel_file, sheet_name, 'K:M', 47, 10)
 df5 = df5.rename(columns={'Unnamed: 10' : 'ETF'})
 df5 = df5.style.format({'Upgrades 1 month': percent_one_decimal, 'Downgrades 1 month': percent_one_decimal})
 st.markdown('### Table 5: Equity ETF - Upgrades')
@@ -140,7 +140,7 @@ st.dataframe(df5, hide_index=True)
 
 
 # TABLE 6
-df6 = load_excel_data(excel_file, sheet_name, 'E:F', 56, 7)
+df6 = load_excel_data(excel_file, sheet_name, 'E:F', 59, 7)
 df6 = df6.rename(columns={'Table 6 : Long Term forecasts ( above local rates )' : 'ETF'})
 df6 = df6.rename(columns={'Unnamed: 5' : 'Long Term Forecasts'})
 df6 = df6.style.format({'Long Term Forecasts': percent_one_decimal})
